db5d08615d7628146b19c51348326e2ada1388b0
mchung03 <minhee.chung14@gmail.com>
2023-09-09 14:59:36 -0700

    removed redux store and created add widget from dropdown menu feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import APODWidget from '../components/widgets/NASA_apod'
-import Bored from '../components/widgets/Bored'
+
 const Home = () => {
     return (
         <div>
         <APODWidget />
-        <Bored />
         </div>
     )
 };
