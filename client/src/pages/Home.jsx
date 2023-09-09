a8b3424565247c49187a75d6c981e7518dc180b2
virginiafreitas <virginiacdefreitas@gmail.com>
2023-09-09 15:50:12 -0700

    solving merge conflict - add joke api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 import APODWidget from '../components/widgets/NASA_apod';
-import jokeWidget from '../components/widgets/JokeAPI';
 
 const Home = () => {
     return (
         <div>
             <APODWidget />;      
-            <jokeWidget />
-=======
-import APODWidget from '../components/widgets/NASA_apod'
-import Bored from '../components/widgets/Bored'
-const Home = () => {
-    return (
-        <div>
-        <APODWidget />
-        <Bored />
->>>>>>> 724dda1278fb90a62491b001bbc37526dc6014ae
         </div>
     )
 };
