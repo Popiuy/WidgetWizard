93290173ae48e21dfb74723ced37f42b093d4867
mchung03 <minhee.chung14@gmail.com>
2023-09-09 12:59:13 -0700

    fixed APOD doc name in home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import APODWidget from '../components/widgets/APOD'
-import BoredAPIWidget from '../components/widgets/Bored';
+import APODWidget from '../components/widgets/NASA_apod'
+import Bored from '../components/widgets/Bored'
 const Home = () => {
     return (
         <div>
         <APODWidget />
-        <BoredAPIWidget />
+        <Bored />
         </div>
-=======
-import APODWidget from '../components/widgets/NASA_apod'
-const Home = () => {
-    return (
-        // <div>TESTING</div>
-        <APODWidget />      
->>>>>>> 514b6b5eea6d82eb8200955ed5b62e1a0238ac8b
     )
 };
 
