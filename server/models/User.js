91eb73dfadad0b9e520bb3e1bf5b123eb10cc9c4
Ryan Payne <137010790+Popiuy@users.noreply.github.com>
2023-09-12 11:09:37 -0700

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,10 @@ const userSchema = new Schema(
         type: String,
         required: true,
       },
+        widgets: {
+            type: Schema.Types.ObjectId,
+            ref: 'Widget'
+        },
     },
     {
       toJSON: {
