a9d850b35dced520d28232eb0a4b4a3323893e23
virginiafreitas <virginiacdefreitas@gmail.com>
2023-09-12 13:52:49 -0700

    import useState and useEffect, create widget function to export

--- client/src/components/Widgets/Webcams-Travel.jsx
+++ client/src/components/Widgets/Webcams-Travel.jsx
@@ -1,3 +1,7 @@
 // https://rapidapi.com/webcams.travel/api/webcams-travel
 // Provides webcams around the world based on lat, long
 // key = gFFaX8hEsYH01S9RYHYwq712ZZs7FOzB
+
+
+import { useState, useEffect } from 'react';
+
