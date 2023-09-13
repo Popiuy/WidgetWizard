3244f72a7fc08371090d7a435aefaf30e0be80a6
Popiuy <rymaxpayne@gmail.com>
2023-09-12 18:06:05 -0700

    added login fix

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,6 +4,7 @@ export const LOGIN_USER = gql`
     mutation login($username: String!, $password: String!){
         login(username: $username, password: $password) {
             token 
+            user{username}
         }
     }
 `;
